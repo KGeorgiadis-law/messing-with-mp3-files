c7daa2044082715b526a637ca08a232da77cf203
Konstantinos Georgiadis <kostas.georgiadis363@gmail.com>
2017-07-30 15:17:55 +0100

    added title functionality

--- mess+with+mp3.py
+++ mess+with+mp3.py
@@ -1,26 +1,13 @@
-
-# coding: utf-8
-
-# In[1]:
-
-#!pip install mutagen
-
-
-# In[3]:
-
 # dependencies
 import os
-
-
-# In[27]:
-
 # for help on mutagen, see https://mutagen.readthedocs.io/en/latest/user/id3.html
 from mutagen.easyid3 import EasyID3
-valid_keys = EasyID3.valid_keys.keys()
-print(valid_keys)   
 
-
-# In[36]:
+# get valid keys
+valid_keys = list(EasyID3.valid_keys.keys())
+valid_keys.sort()
+valid_keys_str = ", ".join(valid_keys)
+print(valid_keys_str)
 
 # functions to use
 def change_mp3_file(mp3_file_folder, mp3_file_name, key_to_change, new_value):
@@ -35,20 +22,15 @@ def change_mp3_file(mp3_file_folder, mp3_file_name, key_to_change, new_value):
     else:
         raise NameError("You doofus! '{}' is not one of the available keys! You can see the available keys by using print(available_keys)".format(key_to_change))
 
-
-# In[34]:
-
 folder_to_use = input("please select root folder: ")
 
-
-# In[ ]:
-
-#get the folder name, which is the same as the album name
-os.chdir(folder_to_use)
-current_working_directory = os.getcwd()
-current_working_directory_list = current_working_directory.split("\\")
-folder_name = current_working_directory_list[-1]
-album_name = folder_name 
+# get the folder name, which is the same as the album name
+# (used when album title is folder title)
+#os.chdir(folder_to_use)
+#current_working_directory = os.getcwd()
+#current_working_directory_list = current_working_directory.split("\\")
+#folder_name = current_working_directory_list[-1]
+#album_name = folder_name 
 
 #all of the above as a one-liner:
 album_name2 = str(os.getcwd()).split("\\")[-1]
@@ -56,20 +38,25 @@ album_name2 = str(os.getcwd()).split("\\")[-1]
 print(album_name)
 #print(album_name2)
 
+print(valid_keys_str)
 
-# In[40]:
+title_file = ""
+
+with open(os.path.join(folder_to_use, title_file), "r") as titles:
+    contents = titles.read()
+    titles_list = contents.split("\n")
+    for i in range(len(titles_list)):
+        print(i, ": ", titles_list[i])
 
 mp3_files = list()
 for root, subdirs, files in os.walk(folder_to_use):
     print("--\nroot = "+root)
     print(subdirs)
     print(files)
+    title_counter = 1
     for file in files:
         if file.endswith(".mp3"):
-            try:
-                change_mp3_file(root, file, "kjasdlkalfd", "")
-            except NameError:
-                print("*** Invalid value to change, script will now exit ***")
-                break
+            change_mp3_file(root, file, "", "")
+        title_counter += 1
 print("Finished!")
 
